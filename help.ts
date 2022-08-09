4ef4de7a9098ee512c5c3725b94086e405d03786
UwUssimo Robinson <uwussimo@icloud.com>
2022-08-09 19:45:58 +0500

    help and simple arg parser

---
+++ help.ts
@@ -0,0 +1,7 @@
+export default `
+makudaun v0.1.0 - simple md to html
+
+makudaun <ARG1> <ARG2>
+    ARG1 - input markdown file (required)
+    ARG2 - output name for html file (optional)
+`
--- mod.ts
+++ mod.ts
@@ -1,8 +1,20 @@
+import help from "./help.ts";
 import styles from "./styles.ts"
 import showdown from "https://esm.sh/showdown@1.9.0";
 
+const input = Deno.args[0]
+const output = Deno.args[1] || "index.html"
+
+if (!input) {
+  console.log(help.trim())
+  Deno.exit(1)
+}
+
 // Read the file
-const markdown = await Deno.readTextFile("./readme.md");
+const markdown = await Deno.readTextFile(input).catch(() => {
+  console.error(`Seems like there is no ${input} file in there...`)
+  Deno.exit(1)
+});
 
 // Converter from md to html
 const converter = new showdown.Converter({
@@ -35,4 +47,4 @@ const postContent = `
 const html = preContent + converter.makeHtml(markdown) + postContent;
 
 // Write converted output to file
-await Deno.writeTextFile("./README.html", html);
+await Deno.writeTextFile(output, html);
